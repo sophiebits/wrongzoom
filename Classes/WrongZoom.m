23643f361892ecc84e22a51c2bfc9e4930fa6555
Ben Alpert <spicyjalapeno@gmail.com>
2009-03-14 17:04:00 -0600

    add Tiger support

--- Classes/WrongZoom.m
+++ Classes/WrongZoom.m
@@ -17,13 +17,14 @@ static NSRect sizer(id self, SEL _cmd, NSWindow *window, NSRect newFrame) {
 static void swizzleSizer(Class class) {
 	Method sizerMethod = class_getInstanceMethod(class, @selector(windowWillUseStandardFrame:defaultFrame:));
 	if(sizerMethod != NULL)
-		method_setImplementation(sizerMethod, (IMP)sizer);
+		sizerMethod->method_imp = (IMP)sizer;
 }
 
 // - (void)zoom:(id)sender
 static IMP oldZoomer;
 static void zoomer(id self, SEL _cmd, id sender) {
-	swizzleSizer([[self delegate] class]);
+	if([self delegate])
+		swizzleSizer([[self delegate] class]);
 	swizzleSizer([self class]);
 
 	oldZoomer(self, _cmd, sender);
@@ -33,8 +34,8 @@ static void zoomer(id self, SEL _cmd, id sender) {
 	NSLog(@"loaded WrongZoom");
 	Class class = NSClassFromString(@"NSWindow");
 	Method zoomMethod = class_getInstanceMethod(class, @selector(zoom:));
-	oldZoomer = method_getImplementation(zoomMethod);
-	method_setImplementation(zoomMethod, (IMP)zoomer);
+	oldZoomer = zoomMethod->method_imp;
+	zoomMethod->method_imp = (IMP)zoomer;
 }
 
 @end
